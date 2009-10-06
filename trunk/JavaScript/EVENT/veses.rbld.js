d2bb26e437f022e5affb8c7ed55edb7d25998e12
tlissak <tlissak@c843539a-804b-11de-8668-3f6947b3de95>
2009-10-06 15:50:50 +0000

    verified by my mik comparing bug fixed by disabling looping of confirmation of hefsek tahara

--- trunk/JavaScript/EVENT/veses.rbld.js
+++ trunk/JavaScript/EVENT/veses.rbld.js
@@ -6,29 +6,22 @@ function calender(){
 calender.prototype._events;
 calender.prototype._veses;
 calender.prototype.vestos_db;
-
-
-var kavuah_reeyahs;
-var kavuah_text;
-var last_veses,before_last_veses;
 var _EVENT_=0;
 var _REEYAH_=1;
 var _HEFSEK_=2;
-
-//var _REPEATING_=3;
-//var _MEMO_=4;
-
 var _CHODESH_=5;
 var _BENONIS_=6;
 var _HAFLAGAH_=7;
 var _KAVUAH_=8;
-
 var _EVENT_NAMES_=new Array('Generic Event','Flow','Hefsek Taharah','Repeating Memo','Memo','Chodesh','Beinonit','Haflagah','Kavuah');
 var _NIGHT_=1;
 var _DAY_=2;
 var _NIGHT_AND_DAY_=3;
 var _ONAH_NAMES_=new Array('','night onah','day onah','night and day onahs');
 var _ONAH_TYPES_=new Array('','sunrise','sunset','sunset and sunrise');
+var kavuah_reeyahs;
+var kavuah_text;
+var last_veses,before_last_veses;
 var Cause ={}
 Cause.start   = 0
 Cause.start_1 = 7
@@ -57,8 +50,7 @@ function Veses(reeyah_date,_time,onah,cause){
 	this._reeyah=reeyah_date;
 	this._time=time2time(_time);
 	if(onah==undefined){
-		var reeyah_day=new Day(reeyah_date);
-		this._onah=reeyah_day.get_onah(this._time);
+		this._onah=reeyah_date.get_onah(this._time);
 	}else{ 
 		this._onah=onah;
 	}
@@ -66,19 +58,16 @@ function Veses(reeyah_date,_time,onah,cause){
 	var hefsek=this.getEarliestHefsekTaharaDate();
 	this._hefsek=hefsek;
 	this._hefsek_confirmed=false;
-	var mikvah=hefsek.clone().add(8);
-	this._mikvah=mikvah;
+	this._mikvah=hefsek.clone().add(8);
 	this._mikvah_confirmed=false;
-	this._id = "new Veses._id"
-	this._leadup_cause = "new Veses"
-	this._leadup_date = "new Veses"
-	this._leadup_onah = "new Veses"
+	this._id = ""
+	this._leadup_cause = ""
+	this._leadup_date = ""
+	this._leadup_onah = ""
 }
-
 Veses.prototype.check_for_bad_mikvah=function(cal){
 	var day;
 	var badMikvah_reason='';
-	//international_dateline_checker(day);
 	var mikvah_day= new HDate(this._mikvah);
 	if(mikvah_day.getDay()==10 && mikvah_day.getMonth() == 7)	
 		badMikvah_reason='since relations are prohibited on Yom Kippur,';
@@ -98,9 +87,9 @@ Veses.prototype.check_for_bad_mikvah=function(cal){
 	}
 	if(badMikvah_reason!='')
 		return "Your mikvah night may have to be postponed until the following night "+badMikvah_reason+" please ask a rabbi how to proceed.";
-	else return'';
+	else 
+		return'';
 }
-
 Veses.prototype.get_prev_veses=function(cal){
 	var last_veses=null;
 	for(i in cal._veses)
@@ -115,9 +104,7 @@ Veses.prototype.StartedInWhiteWeek=function(cal){
 	return (last_veses._mikvah.gt(this._reeyah))
 }
 Veses.prototype.info=function(){
-	return "Vesos starting on "+this._reeyah+", ht on "+this._hefsek+", mikvah night iy\"h on "+this._mikvah;}
-	
-	
+	return "Vesos starting on "+this._reeyah+", ht on "+this._hefsek+", mikvah night iy\"h on "+this._mikvah;}	
 Veses.prototype.set_haflagas=function(cal,last_veses){
 	for(i in this._haflagas){
 		var _date=this._hefsek.clone();
@@ -141,30 +128,24 @@ Veses.prototype.set_haflagas=function(cal,last_veses){
 		}
 	}
 }
-
-
-
-
 Veses.prototype.getEarliestHefsekTaharaNumber=function(){
 	var v=this;
-	if(v.StartedInWhiteWeek(oCal)||v._cause==Cause.birth_s){
+	if(v.StartedInWhiteWeek(Cal_Veses)||v._cause==Cause.birth_s){
 		min_ht=0;
 	}else if(v._leadup_date!==undefined){
 		days_already_red=1;
 		kessem=v._leadup_date.clone();
-		//**************************************
 		while(v._reeyah.gt(kessem)){
 			days_already_red++;
 			kessem.nextDay();
 		}
-		//***************************************/
 		min_ht=5-days_already_red;
 		if(min_ht<0){
 			mint_ht=0;
 		}
 	}else if(v._cause==Cause.birth_d){
 		min_ht=6;
-	}else if(v._cause==Cause.preglost/*&&!v.goesOnCalendar()*/){
+	}else if(v._cause==Cause.preglost&&!v.goesOnCalendar()){
 		min_ht=6;
 	}else{
 		min_ht=4;
@@ -177,10 +158,8 @@ Veses.prototype.getEarliestHefsekTaharaDate=function(){
 	var earliest_ht_date=v._reeyah.clone().add(min_ht);
 	return earliest_ht_date;
 }
-
 Veses.prototype.confirm_hefsek=function(cal){
-	// check if hefsek is future date return false
-	//store it in vestos_db top.frame[1].vestos_db=new Array();
+	// check if hefsek is future date return false 	//store it in vestos_db top.frame[1].vestos_db=new Array();
 	//if(this._hefsek.gt(now)){popup("Please ensure the Hefsek Taharah is not a future date");return false;}
 	this._hefsek_confirmed=true;
 	var mikvah=this._hefsek.clone().add(8);
@@ -192,39 +171,26 @@ Veses.prototype.confirm_hefsek=function(cal){
 			this._mikvah=earliest_mikvah;
 		}
 	}
-	
-		
-	//add_reminder("7 nekiim")
-	
+	//add_reminder("7 nekiim")	
 	var bad_mikvah=this.check_for_bad_mikvah(cal);
-	if (bad_mikvah != ''){
-	//	add_reminder("Veses.confirm_hefsek ::  BAD MIKVEH reminder-------------------")
+	if (bad_mikvah != ''){//	add_reminder("Veses.confirm_hefsek ::  BAD MIKVEH reminder-------------------")
 		this._mikvah_confirmed=false
 	}else{
-		this._mikvah_confirmed=true
-	//	add_reminder("Veses.prototype.confirm_hefsek :: add reminder for the mikveh")
+		this._mikvah_confirmed=true //	add_reminder("Veses.prototype.confirm_hefsek :: add reminder for the mikveh")
 	}
 	var last_veses=this.get_prev_veses(cal);
-	/******************************************		BUGY */
 	while(last_veses!=null&&!last_veses.goesOnCalendar()){		last_veses=last_veses.get_prev_veses(cal);}
 	
 	this.set_haflagas(cal,last_veses);//set haflaga for the last veses
-	var chashashot=new Array();
-	
-	
+	var chashashot=new Array();	
 	// find older chashashot inside bleeding days
 	var date=this._reeyah.clone();
 	// if raia < hefsek == its sure
-	if(!date.gt(this._hefsek)){	
-		// loop from reeyah to hefsek (bleeding days)
-		//**************************************************
+	if(!date.gt(this._hefsek)){	// loop from reeyah to hefsek (bleeding days)
 		while(!date.gt(this._hefsek)){
-			for(i in cal._events){
-				// if one of chashashot inside the bleeding days is yom ha chodesh
+			for(i in cal._events){	// if one of chashashot inside the bleeding days is yom ha chodesh
 				if(cal._events[i]._date.eq(date)&&cal._events[i]._type==_CHODESH_){
-					// if current raia is not the bleeding day
-					// or current raia is the date and the chashashot 
-					// can be day or night
+					// if current raia is not the bleeding day or current raia is the date and the chashashot can be day or night
 					if( ! this._reeyah.eq(date)	||	(this._reeyah.eq(date)&&(cal._events[i]._onah==_DAY_||this._onah==_NIGHT_))		){
 							chashashot.push(cal._events[i]);
 					}
@@ -232,34 +198,24 @@ Veses.prototype.confirm_hefsek=function(cal){
 			}
 			date.nextDay();
 		}
-		//***********************************************/
+		//**********************************************
 	}
-	// loop chashashot inside bleeding days 
-	// and to next month 
+	// loop chashashot inside bleeding days and to next month 
 	for(i in chashashot){
-		var next_month=chashashot[i]._date.clone().nextMonth();
-		// if the next month dosent have a 30 days so movenext chashash
-		
+		var next_month=chashashot[i]._date.clone().nextMonth(); //if the next month dosent have a 30 days so movenext chashash
 		if(chashashot[i]._date.getDay()==30&&next_month.getMonthLength()==29) {
-			//console.log("innnnn- 30")
 			continue;
 		}
 		//*********** to better understand
-		//
-		// if the chashash inside bleeding day has veses caller
-		// so copy the chashash to next month with this caller
+		// if the chashash inside bleeding day has veses caller  so copy the chashash to next month with this caller
 		// else copy the chashsh to next month without caller of this bot with the same ona
 		if(chashashot[i]._veses!=null){
 			var _event=new Event(next_month,_CHODESH_,this);
 		}else {
 			var _event=new Event(next_month,_CHODESH_,null);
 			_event._onah=chashashot[i]._onah;
-		}
-		
-		//if the chashsh has caller veses but not misc
-		// add misc from veses
-		// else the misc is the same misc of chashsh
-		
+		}		
+		//if the chashsh has caller veses but not misc add misc from veses else the misc is the same misc of chashsh		
 		if(chashashot[i]._misc==null&&chashashot[i]._veses!=null){
 			_event._misc=chashashot[i]._veses;
 		}else{
@@ -273,11 +229,12 @@ Veses.prototype.set_haflagah_list=function(haflagah){
 		this._haflagas=new Array();
 		var h=haflagah.split(",");
 		for(i in h){
-			this._haflagas.push(new Array(h[i].replace(" ",""),1,this._reeyah.clone()));
+			ynew_haflaga = new Array(h[i].replace(" ",""),1,this._reeyah.clone())
+			this._haflagas.push(ynew_haflaga);
+			console.info("adding new HF 0",ynew_haflaga)
 		}
 	}
 }
-
 Veses.prototype.goesOnCalendar=function(){
 	if(this._goesOnCal!==undefined){ return this._goesOnCal;}
 	if(this._cause==1||this._cause==2||this._cause==4||this._cause==5){
@@ -285,8 +242,8 @@ Veses.prototype.goesOnCalendar=function(){
 		return false;
 	}
 	if(this._cause==3){
-		last_v=this.get_prev_veses(oCal);
-		if(this.StartedInWhiteWeek(oCal)&&(last_v==null||last_v.goesOnCalendar())){
+		last_v=this.get_prev_veses(Cal_Veses);
+		if(this.StartedInWhiteWeek(Cal_Veses)&&(last_v==null||last_v.goesOnCalendar())){
 			this._goesOnCal=false;
 			return false;
 		}else {
@@ -294,13 +251,13 @@ Veses.prototype.goesOnCalendar=function(){
 			return true;
 		}
 	}
-	last_v=this.get_prev_veses(oCal);
+	last_v=this.get_prev_veses(Cal_Veses);
 	if(last_v==null){
 		this._goesOnCal=true;
 		return true;
 	}
 	while(!last_v.goesOnCalendar()){
-		last_v=last_v.get_prev_veses(oCal);
+		last_v=last_v.get_prev_veses(Cal_Veses);
 		if(last_v==null){
 			return true;
 		}
